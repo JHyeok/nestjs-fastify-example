dcdcd6431d78d0d7760a47e95d9ae3d851ee0863
JaeHyeok Kim <dev.jhyeok@gmail.com>
2023-07-30 19:01:14 +0900

    Initial commit

---
+++ test/unit/service/user.service.unit.spec.ts
@@ -0,0 +1,124 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { User } from 'src/api/user/domain/user.entity';
+import { UserService } from 'src/api/user/user.service';
+import { UserCreateRequestDto } from 'src/api/user/dto/user-create-request.dto';
+import { UserUpdateRequestDto } from 'src/api/user/dto/user-update-request.dto';
+import { NotFoundException } from '@nestjs/common';
+import { UserMessage } from 'src/api/user/user.message';
+import { UserRepository } from 'src/api/user/user.repository';
+import { DeleteResult } from 'typeorm';
+
+describe('UserService (Unit)', () => {
+  let userService: UserService;
+  let userRepository: UserRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UserService, UserRepository],
+    }).compile();
+
+    userService = module.get<UserService>(UserService);
+    userRepository = module.get<UserRepository>(UserRepository);
+  });
+
+  describe('create', () => {
+    it('유저를 생성하고, 생성한 유저를 반환한다', async () => {
+      const firstName = '재혁';
+      const lastName = '김';
+      const requestDto = UserCreateRequestDto.of(firstName, lastName);
+      const savedUser = User.of(firstName, lastName, true);
+      jest.spyOn(userRepository, 'save').mockResolvedValue(savedUser);
+
+      const result = await userService.create(requestDto);
+
+      expect(result).toBe(savedUser);
+    });
+  });
+
+  describe('findAll', () => {
+    it('생성된 모든 유저 목록을 반환한다', async () => {
+      const existingUserList = [
+        User.of('재혁', '김', true),
+        User.of('길동', '홍', true),
+      ];
+      jest.spyOn(userRepository, 'find').mockResolvedValue(existingUserList);
+
+      const result = await userService.findAll();
+
+      expect(result).toStrictEqual(existingUserList);
+    });
+  });
+
+  describe('findById', () => {
+    it('생성되지 않은 유저의 id가 주어진다면 유저를 찾을 수 없다는 예외를 던진다', async () => {
+      const userId = 1;
+      jest.spyOn(userRepository, 'findOneByUserId').mockResolvedValue(null);
+
+      const result = async () => {
+        await userService.findById(userId);
+      };
+
+      await expect(result).rejects.toThrowError(
+        new NotFoundException(UserMessage.NOT_FOUND_USER),
+      );
+    });
+
+    it('생성된 유저의 id가 주어진다면 해당 id의 유저를 반환한다', async () => {
+      const userId = 1;
+      const existingUser = User.of('재혁', '김', true);
+      jest
+        .spyOn(userRepository, 'findOneByUserId')
+        .mockResolvedValue(existingUser);
+
+      const result = await userService.findById(userId);
+
+      expect(result.firstName).toBe('재혁');
+      expect(result.lastName).toBe('김');
+    });
+  });
+
+  describe('update', () => {
+    it('생성되지 않은 유저의 id가 주어진다면 유저를 찾을 수 없다는 예외를 던진다', async () => {
+      const userId = 1;
+      const requestDto = UserUpdateRequestDto.of('길동', '김', false);
+      jest.spyOn(userRepository, 'findOneByUserId').mockResolvedValue(null);
+
+      const result = async () => {
+        await userService.update(userId, requestDto);
+      };
+
+      await expect(result).rejects.toThrowError(
+        new NotFoundException(UserMessage.NOT_FOUND_USER),
+      );
+    });
+
+    it('생성된 유저의 id가 주어진다면 해당 id의 유저를 수정하고 수정된 유저를 반환한다', async () => {
+      const userId = 1;
+      const lastName = '김';
+      const requestDto = UserUpdateRequestDto.of('길동', lastName, false);
+      const existingUser = User.of('재혁', lastName, true);
+      const savedUser = User.of('길동', lastName, false);
+      jest
+        .spyOn(userRepository, 'findOneByUserId')
+        .mockResolvedValue(existingUser);
+      jest.spyOn(userRepository, 'save').mockResolvedValue(savedUser);
+
+      const result = await userService.update(userId, requestDto);
+
+      expect(result).toBe(savedUser);
+    });
+  });
+
+  describe('delete', () => {
+    it('생성된 유저의 id가 주어진다면 생성된 유저를 삭제한다', async () => {
+      const userId = 1;
+      jest
+        .spyOn(userRepository, 'delete')
+        .mockResolvedValue({} as DeleteResult);
+
+      const result = await userService.delete(userId);
+
+      expect(result).toBeUndefined();
+    });
+  });
+});
